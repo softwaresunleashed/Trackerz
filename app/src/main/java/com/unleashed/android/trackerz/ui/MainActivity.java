c44236d64d2cc75a3f8f3e877bf97189dca56c8f
Sudhanshu Gupta <softwares.unleashed@gmail.com>
2015-12-13 08:58:35 +0530

    13 Dec 2015 - Added sample code for retrofit

--- app/src/main/java/com/unleashed/android/trackerz/ui/MainActivity.java
+++ app/src/main/java/com/unleashed/android/trackerz/ui/MainActivity.java
@@ -13,13 +13,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.unleashed.android.trackerz.API.gitapi;
 import com.unleashed.android.trackerz.LoginModule.LoginActivity;
+import com.unleashed.android.trackerz.Maps.MapsActivity;
 import com.unleashed.android.trackerz.R;
 import com.unleashed.android.trackerz.Settings.SettingsActivity;
-import com.unleashed.android.trackerz.Maps.MapsActivity;
+import com.unleashed.android.trackerz.model.gitmodel;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 
 public class MainActivity extends AppCompatActivity
@@ -27,12 +37,64 @@ public class MainActivity extends AppCompatActivity
 
     private long back_pressed = 0;   // Counter to keep track of time elapsed between previous back button pressed.
 
+    Button click;
+    TextView tv;
+    EditText edit_user;
+    ProgressBar pbar;
+    String API = "https://api.github.com";                         //BASE URL
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        /////// Retrofit ////////
+
+        click = (Button) findViewById(R.id.button);
+        tv = (TextView) findViewById(R.id.tv);
+        edit_user = (EditText) findViewById(R.id.edit);
+        pbar = (ProgressBar) findViewById(R.id.pb);
+        pbar.setVisibility(View.INVISIBLE);
+
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edit_user.getText().toString();
+                pbar.setVisibility(View.VISIBLE);
+
+                //Retrofit section start from here...
+                RestAdapter restAdapter = new RestAdapter.Builder()
+                        .setEndpoint(API).build();                                        //create an adapter for retrofit with base url
+
+                gitapi git = restAdapter.create(gitapi.class);                            //creating a service for adapter with our GET class
+
+                //Now ,we need to call for response
+                //Retrofit using gson for JSON-POJO conversion
+
+                git.getFeed(user, new Callback<gitmodel>() {
+                    @Override
+                    public void success(gitmodel gitmodel, Response response) {
+                        //we get json object from github server to our POJO or model class
+
+                        tv.setText("Github Name :" + gitmodel.getName() + "\nWebsite :" + gitmodel.getBlog() + "\nCompany Name :" + gitmodel.getCompany());
+
+                        pbar.setVisibility(View.INVISIBLE);                               //disable progressbar
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+                        tv.setText(error.getMessage());
+                        pbar.setVisibility(View.INVISIBLE);                               //disable progressbar
+                    }
+                });
+            }
+        });
+
+
+        /////////////////////////
 
         // Main Activity FAB button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
