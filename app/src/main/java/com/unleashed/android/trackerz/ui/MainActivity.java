495c25f6059c8deac93298179451d0a94ac6fe9a
Sudhanshu Gupta <sudhanshu.gupta@olx.com>
2015-12-14 11:04:45 +0530

    14 Dec 2015 - Corrected and commited silly mistakes of code merge

--- app/src/main/java/com/unleashed/android/trackerz/ui/MainActivity.java
+++ app/src/main/java/com/unleashed/android/trackerz/ui/MainActivity.java
@@ -19,12 +19,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.unleashed.android.trackerz.API.gitapi;
+
 import com.unleashed.android.trackerz.LoginModule.LoginActivity;
 import com.unleashed.android.trackerz.Maps.MapsActivity;
 import com.unleashed.android.trackerz.R;
 import com.unleashed.android.trackerz.Settings.SettingsActivity;
-import com.unleashed.android.trackerz.model.gitmodel;
+
 
 import retrofit.Callback;
 import retrofit.RestAdapter;
@@ -50,51 +50,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        /////// Retrofit ////////
-
-        click = (Button) findViewById(R.id.button);
-        tv = (TextView) findViewById(R.id.tv);
-        edit_user = (EditText) findViewById(R.id.edit);
-        pbar = (ProgressBar) findViewById(R.id.pb);
-        pbar.setVisibility(View.INVISIBLE);
-
-        click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String user = edit_user.getText().toString();
-                pbar.setVisibility(View.VISIBLE);
-
-                //Retrofit section start from here...
-                RestAdapter restAdapter = new RestAdapter.Builder()
-                        .setEndpoint(API).build();                                        //create an adapter for retrofit with base url
-
-                gitapi git = restAdapter.create(gitapi.class);                            //creating a service for adapter with our GET class
-
-                //Now ,we need to call for response
-                //Retrofit using gson for JSON-POJO conversion
-
-                git.getFeed(user, new Callback<gitmodel>() {
-                    @Override
-                    public void success(gitmodel gitmodel, Response response) {
-                        //we get json object from github server to our POJO or model class
-
-                        tv.setText("Github Name :" + gitmodel.getName() + "\nWebsite :" + gitmodel.getBlog() + "\nCompany Name :" + gitmodel.getCompany());
-
-                        pbar.setVisibility(View.INVISIBLE);                               //disable progressbar
-                    }
-
-                    @Override
-                    public void failure(RetrofitError error) {
-                        tv.setText(error.getMessage());
-                        pbar.setVisibility(View.INVISIBLE);                               //disable progressbar
-                    }
-                });
-            }
-        });
-
-
-        /////////////////////////
 
         // Main Activity FAB button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
